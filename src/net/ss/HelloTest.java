8fa5c55ae54f4f2307e710fb3748f763fb3968fd
thekyung2 <runthekyunga@gmail.com>
2012-11-12 21:08:15 +0900

    add getMeassage()

--- src/net/ss/HelloTest.java
+++ src/net/ss/HelloTest.java
@@ -3,5 +3,8 @@ package net.ss;
 import junit.framework.TestCase;
 
 public class HelloTest extends TestCase {
-
+	public void testGetMessage(){
+		HelloWorld hello = new HelloWorld();
+		assertEquals("Hello World",hello.getMessage());
+	}
 }
---
+++ src/net/ss/HelloWorld.java
@@ -0,0 +1,10 @@
+package net.ss;
+
+public class HelloWorld {
+
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return "Hello World";
+	}
+
+}
